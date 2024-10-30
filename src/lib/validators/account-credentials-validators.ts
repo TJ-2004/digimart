10709f7807e918a6109cc9d1585e59afa95dee11
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-10-30 23:11:32 +0530

    🚩feat:Implement auth flow(sign-up)

---
+++ src/lib/validators/account-credentials-validators.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const AuthCredentialsValidator = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be atleast 8 charcters long." }),
+});
+export type TAuthCredentialsValidator = z.infer<
+  typeof AuthCredentialsValidator
+>;
