f93155ec7e634495d6c5642b8e384f0c63c632db
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-11-03 23:00:04 +0530

    🚀feat:trpc setup

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -12,6 +12,7 @@ import {
   AuthCredentialsValidator,
   TAuthCredentialsValidator,
 } from "@/lib/validators/account-credentials-validators";
+import { trpc } from "@/trpc/client";
 
 const Page = () => {
   const {
@@ -21,6 +22,8 @@ const Page = () => {
   } = useForm<TAuthCredentialsValidator>({
     resolver: zodResolver(AuthCredentialsValidator),
   });
+  const { data } = trpc.anyApiRoute.useQuery();
+  console.log(data);
   const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
     //send data to server
   };
@@ -30,7 +33,7 @@ const Page = () => {
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col items-center space-y-2 text-center">
             <Icons.logo className="h-20 w-20" />
-            <h1 className="text-2xl font-bold">Crete an account</h1>
+            <h1 className="text-2xl font-bold">Create an account</h1>
             <Link
               href="/sign-in"
               className={buttonVariants({
